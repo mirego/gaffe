db0c6d45036f1ea188aa3cdb8e9e3f92024104fd
Rémi Prévost <remi@exomel.com>
2013-07-14 12:10:29 -0400

    Add documentation to a few methods

--- lib/gaffe.rb
+++ lib/gaffe.rb
@@ -2,23 +2,28 @@ require 'gaffe/version'
 require 'gaffe/errors'
 
 module Gaffe
+  # Yield a block to populate @configuration
   def self.configure
     yield configuration
   end
 
+  # Return the configuration settings
   def self.configuration
     @configuration ||= OpenStruct.new
   end
 
+  # Return either the user-defined controller or our default controller
   def self.errors_controller
     @errors_controller ||= (configuration.errors_controller || builtin_errors_controller)
   end
 
+  # Return our default controller
   def self.builtin_errors_controller
     require 'gaffe/errors_controller'
     Gaffe::ErrorsController
   end
 
+  # Configure Rails to use our code when encountering exceptions
   def self.enable!
     Rails.application.config.exceptions_app = lambda do |env|
       Gaffe.errors_controller.action(:show).call(env)
